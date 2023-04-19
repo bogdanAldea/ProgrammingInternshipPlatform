f0d7553384e741bdb2ad7a8037306361fbdaf18e
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-04-19 13:25:29 +0300

    Created validation attributes.

--- backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Api/Contracts/InternshipContracts/Requests/InternshipPostDto.cs
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Api/Contracts/InternshipContracts/Requests/InternshipPostDto.cs
@@ -1,10 +1,25 @@
-﻿namespace ProgrammingInternshipPlatform.Api.Contracts.InternshipContracts.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+using ProgrammingInternshipPlatform.Api.RequestAttributes;
+using ProgrammingInternshipPlatform.Domain.Shared.Validators;
+
+namespace ProgrammingInternshipPlatform.Api.Contracts.InternshipContracts.Requests;
 
 public class InternshipPostDto
 {
+    [Required]
     public Guid LocationId { get; set; }
+    
+    [Required]
+    [MinValue(RuleConstants.InternshipConstants.MinInternsToEnrol)]
     public int MaximumInternsToEnroll { get; set; }
+    
+    [Required]
+    [MinValue(RuleConstants.InternshipConstants.MinDurationInMonths)]
     public int DurationInMonths { get; set; }
+    
+    [Required]
     public DateTime ScheduledToStartOnDate { get; set; }
+    
+    [Required]
     public DateTime ScheduledToEndOnDate { get; set; }
 }
--- backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Domain/Internships/Validators/TimeframeValidator.cs
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Domain/Internships/Validators/TimeframeValidator.cs
@@ -9,12 +9,14 @@ public class TimeframeValidator : DomainAbstractValidator<Timeframe>
     public TimeframeValidator()
     {
         RuleFor(timeframe => timeframe.ScheduledToStartOn)
+            .Cascade(CascadeMode.Stop)
             .NotEmpty()
             .WithMessage(RuleFailureMessages.TimeframeMessages.EmptyOrNullProperty)
             .Must(startDate => startDate >= DateTime.Today)
             .WithMessage(RuleFailureMessages.TimeframeMessages.StartDateSetInThePast);
 
         RuleFor(timeframe => timeframe.ScheduledToEndOn)
+            .Cascade(CascadeMode.Stop)
             .NotEmpty()
             .WithMessage(RuleFailureMessages.TimeframeMessages.EmptyOrNullProperty)
             .Must((timeframe, endDate) => endDate > timeframe.ScheduledToStartOn)

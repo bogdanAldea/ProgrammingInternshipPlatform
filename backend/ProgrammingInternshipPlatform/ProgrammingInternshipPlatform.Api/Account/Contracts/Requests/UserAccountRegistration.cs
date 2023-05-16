9fe9cfd97f0cef8d0ca862ec236d0d738497594d
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-05-16 22:32:17 +0300

    Added endpoint for registering new users.

---
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Api/Account/Contracts/Requests/UserAccountRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammingInternshipPlatform.Api.Account.Contracts.Requests;
+
+public class UserAccountRegistration
+{
+    public UserAccountRegistration(string firstName, string lastName, 
+        string email, string password, string? pictureUrl, Guid companyId)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+        PictureUrl = pictureUrl;
+        CompanyId = companyId;
+    }
+
+    [Required]
+    public string FirstName { get; }
+    [Required]
+    public string LastName { get; }
+    [Required]
+    public string Email { get; }
+    [Required]
+    public string Password { get; }
+    public string? PictureUrl { get; }
+    [Required]
+    public Guid CompanyId { get; }
+}
---
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Api/Account/Controllers/UserAccountsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammingInternshipPlatform.Api.Account.Contracts.Requests;
+using ProgrammingInternshipPlatform.Api.Account.Contracts.Responses;
+using ProgrammingInternshipPlatform.Api.Controllers;
+using ProgrammingInternshipPlatform.Application.Account;
+using ProgrammingInternshipPlatform.Domain.Organization.Company;
+
+namespace ProgrammingInternshipPlatform.Api.Account.Controllers;
+
+public class UserAccountsController : ApiController
+{
+    [HttpPost]
+    public async Task<IActionResult> RegisterUserAccount([FromBody] UserAccountRegistration userAccountRegistration)
+    {
+        var userAccountRegistrationCommand = new RegisterNewAccountCommand(FirstName: userAccountRegistration.FirstName,
+            LastName: userAccountRegistration.LastName, Email: userAccountRegistration.Email,
+            Password: userAccountRegistration.Password, PictureUrl: userAccountRegistration.PictureUrl,
+            CompanyId: new CompanyId(userAccountRegistration.CompanyId));
+
+        var handlerResult = await Mediator.Send(userAccountRegistrationCommand);
+        if (handlerResult.IsSuccess)
+        {
+            var userAccountResponse = UserAccountDetails.MapFromUserAccount(handlerResult.Payload!);
+            return Ok(userAccountResponse);
+        }
+
+        return HandleApiErrorResponse(handlerResult.FailureReason);
+    }
+}

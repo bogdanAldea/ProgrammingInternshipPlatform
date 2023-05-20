56673738c8a47d000c2183d4a17fd4227a3c4cb2
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-05-20 12:37:51 +0300

    Added methods to aggregate root Board.

--- backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Domain/Backlog/Boards/Board.cs
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Domain/Backlog/Boards/Board.cs
@@ -6,11 +6,48 @@ namespace ProgrammingInternshipPlatform.Domain.Backlog.Boards;
 
 public class Board
 {
+    private static readonly BoardValidator BoardValidator = new();
     private readonly List<Stage> _stages = new();
-    
+
     public BoardId BoardId { get; private set; }
     public ProjectId ProjectId { get; private set; }
     public InternId OwnerIntern { get; private set; }
     public string Title { get; private set; } = null!;
     public IReadOnlyCollection<Stage> Stages => _stages;
+
+    public static async Task<Board> CreateNew(ProjectId projectId, InternId ownerIntern, string projectTitle,
+        CancellationToken cancellationToken)
+    {
+        var boardToCreate = new Board
+        {
+            BoardId = new BoardId(Guid.NewGuid()),
+            ProjectId = projectId,
+            OwnerIntern = ownerIntern,
+            Title = $"{projectTitle} Backlog"
+        };
+        await BoardValidator.ValidateDomainModelAsync(boardToCreate, cancellationToken);
+        return boardToCreate;
+    }
+
+    public async Task AddStage(string title, int? order, CancellationToken cancellationToken)
+    {
+        order ??= _stages.Count + 1;
+        var stageToAdd = await Stage.CreateNew(BoardId, title, (int)order, cancellationToken);
+        _stages.Add(stageToAdd);
+        await BoardValidator.ValidateDomainModelAsync(this, cancellationToken);
+    }
+
+    public async Task RemoveStage(StageId stageId, CancellationToken cancellationToken)
+    {
+        var stageToRemove = _stages.FirstOrDefault(stage => stage.StageId == stageId);
+        if (stageToRemove != null) _stages.Remove(stageToRemove);
+        await BoardValidator.ValidateDomainModelAsync(this, cancellationToken);
+    }
+
+    public async Task ChangeStageTitle(StageId stageId, string newTitle, CancellationToken cancellationToken)
+    {
+        var stageToUpdateTitleTo = _stages.FirstOrDefault(stage => stage.StageId == stageId);
+        if (stageToUpdateTitleTo != null) await stageToUpdateTitleTo.ChangeTitle(newTitle, cancellationToken);
+        await BoardValidator.ValidateDomainModelAsync(this, cancellationToken);
+    }
 }
--- backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Domain/Backlog/Stages/Stage.cs
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Domain/Backlog/Stages/Stage.cs
@@ -5,6 +5,7 @@ namespace ProgrammingInternshipPlatform.Domain.Backlog.Stages;
 
 public class Stage
 {
+    private static readonly StageValidator StageValidator = new();
     private readonly List<Card> _cards = new();
 
     public Stage() {}
@@ -14,4 +15,29 @@ public class Stage
     public string Title { get; private set; } = null!;
     public int Order { get; private set; }
     public IReadOnlyCollection<Card> Cards => _cards;
+
+    public static async Task<Stage> CreateNew(BoardId boardId, string title, int order, CancellationToken cancellationToken)
+    {
+        var stageToCreate = new Stage
+        {
+            StageId = new StageId(Guid.NewGuid()),
+            BoardId = boardId,
+            Title = title,
+            Order = order
+        };
+        await StageValidator.ValidateDomainModelAsync(stageToCreate, cancellationToken);
+        return stageToCreate;
+    }
+
+    public async Task ChangeTitle(string newTitle, CancellationToken cancellationToken)
+    {
+        Title = newTitle;
+        await StageValidator.ValidateDomainModelAsync(this, cancellationToken);
+    }
+
+    public async Task ChangeOrder(int newOrder, CancellationToken cancellationToken)
+    {
+        Order = newOrder;
+        await StageValidator.ValidateDomainModelAsync(this, cancellationToken);
+    }
 }

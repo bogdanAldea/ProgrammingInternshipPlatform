971000447de8a11583f14d09aa6fb0ed0b1791ee
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-06-29 16:25:36 +0300

    Created registration page.

---
+++ client/Platform/src/app/features/authencation/authencation.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminRegistrationComponent } from './admin-registration/admin-registration.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminRegistrationComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class AuthencationModule { }

14563b552ffe2109541467ec26b63e3ed9e3150b
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-07-19 18:42:11 +0300

    Added api logic to assign and remove roles.

---
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Api/Account/Contracts/Requests/RoleRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace ProgrammingInternshipPlatform.Api.Account.Contracts.Requests;
+
+public class RoleRequest
+{
+    public string Name { get; set; } = null!;
+    public string Id { get; set; } = null!;
+}
---
+++ backend/ProgrammingInternshipPlatform/ProgrammingInternshipPlatform.Application/Account/RemoveRolesFromUserAccount.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ProgrammingInternshipPlatform.Application.ResultPattern;
+using ProgrammingInternshipPlatform.Domain.Account.UserAccounts;
+
+namespace ProgrammingInternshipPlatform.Application.Account;
+
+public record RemoveRolesFromUserAccountCommand(Guid IdentityId, IEnumerable<string> Roles)
+    : IRequest<HandlerResult<UserAccount>>;
+
+public class RemoveRolesFromUserAccountHandler 
+    : IRequestHandler<RemoveRolesFromUserAccountCommand, HandlerResult<UserAccount>>
+{
+    private readonly UserManager<IdentityUser> _context;
+
+    public RemoveRolesFromUserAccountHandler(UserManager<IdentityUser> context)
+    {
+        _context = context;
+    }
+    
+    public async Task<HandlerResult<UserAccount>> Handle(RemoveRolesFromUserAccountCommand request, CancellationToken cancellationToken)
+    {
+        var accountIdentity = await _context.FindByIdAsync(request.IdentityId.ToString());
+        if (accountIdentity is null)
+        {
+            var identityNotFoundError =
+                ApplicationError.NotFoundFailure(ApplicationErrorMessages.UserAccount.AccountIdentityNotFound);
+            return HandlerResult<UserAccount>.Fail(identityNotFoundError);
+        }
+
+        await _context.RemoveFromRolesAsync(accountIdentity, request.Roles);
+        return HandlerResult<UserAccount>.Success();
+    }
+}

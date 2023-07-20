09e37629f9c2365db1aa21f6cae9e89fa694b563
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-07-20 14:35:52 +0300

    Refactored client

--- client/src/app/shared/dialogs/user-role-dialog/user-role-dialog.component.ts
+++ client/src/app/shared/dialogs/user-role-dialog/user-role-dialog.component.ts
@@ -1,11 +1,9 @@
 import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { DeltaRolesRequest } from 'src/app/core/accounts/contracts/DeltaRolesRequest';
 import { AccountWithRoles } from 'src/app/core/accounts/contracts/account-with-roles';
 import { Role } from 'src/app/core/accounts/contracts/role';
-import { UserData } from 'src/app/core/accounts/models/user-data';
 import { AccountService } from 'src/app/core/accounts/services/account-service/account.service';
 import { RolesService } from 'src/app/core/accounts/services/roles-service/roles.service';
 
@@ -17,23 +15,25 @@ import { RolesService } from 'src/app/core/accounts/services/roles-service/roles
 export class UserRoleDialogComponent implements OnInit{
   @Output() userRolesChangedEvent: EventEmitter<DeltaRolesRequest> = new EventEmitter<DeltaRolesRequest>;
   public userAccount$: Observable<AccountWithRoles> | undefined;
-  public userRoles: Role[] | undefined;
   public roles$: Observable<Role[]> | undefined;
+  public userRoles: Role[] | undefined;
   public deltaRoleChange: DeltaRolesRequest = { rolesToAdd: [], rolesToRemoved: [] }
   
   public constructor(
-    @Inject(MAT_DIALOG_DATA) public data: UserData,
+    @Inject(MAT_DIALOG_DATA) public data: any,
     private accountService: AccountService,
     private roleService: RolesService,
-  ) {}
+  ) 
+  {
+    this.userAccount$ = data.selectedUserAccount,
+    this.roles$ = data.systemRoles
+  }
   
   public ngOnInit(): void {
-    console.log(this.data.userId)
-    this.userAccount$ = this.getUserAccount(this.data.userId);
-    this.userAccount$.subscribe(account => {
+    this.userAccount$?.subscribe(account => {
       this.userRoles = account.roles;
     })
-    this.roles$ = this.getRoles();
+
   }
 
   public isRoleAssignedToUser = (role: Role) => {

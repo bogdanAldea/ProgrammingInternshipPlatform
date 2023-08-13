4255000b9e86e34023666c59f9fd15035cb49962
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-08-13 23:07:45 +0300

    Removed old navigations

--- client/src/app/shared-modules/navigations/navigations.module.ts
+++ client/src/app/shared-modules/navigations/navigations.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserControlsNavigationComponent } from './user-controls-navigation/user-controls-navigation.component';
-import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 import { RouterModule } from '@angular/router';
 import { NavLinkComponent } from './nav-link/nav-link.component';
 import { AvatarsModule } from '../avatars/avatars.module';
@@ -13,8 +11,6 @@ import { InlineNavigationComponent } from './inline-navigation/inline-navigation
 
 @NgModule({
   declarations: [
-    UserControlsNavigationComponent,
-    NavigationMenuComponent,
     NavLinkComponent,
     InlineNavigationComponent
   ],
@@ -25,8 +21,6 @@ import { InlineNavigationComponent } from './inline-navigation/inline-navigation
     MatInputModule
   ],
   exports: [
-    UserControlsNavigationComponent,
-    NavigationMenuComponent,
     InlineNavigationComponent
   ]
 })

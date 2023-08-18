8ae3f97bbcbc466d1fd271b4e73e089a0efda74d
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-08-18 10:23:25 +0300

    Added new icons

--- client/src/app/views/application-configs/icon-registrar/IconRegistrar.ts
+++ client/src/app/views/application-configs/icon-registrar/IconRegistrar.ts
@@ -6,27 +6,32 @@ export namespace IconRegistrar {
     const dark = `${iconsPath}/dark`
     const light = `${iconsPath}/light`
     const brand = `${iconsPath}/purple`
+    const muted = `${iconsPath}/muted`;
 
     export class  Presentation {
         public static Dark = `${dark}/Menu/Presentations.svg`
+        public static Muted = `${muted}/Menu/Presentations.svg`
         public static Light = ''
         public static Brand = ''
     }
 
     export class Location {
         public static Dark = `${dark}/Global/Location.svg`;
+        public static Muted = `${muted}/Global/Location.svg`
         public static Light = '';
         public static Brand = '';
     }
 
     export class Users {
         public static Dark = `${dark}/Global/Users.svg`;
+        public static Muted = `${muted}/Global/Users.svg`
         public static Light = '';
         public static Brand = '';
     }
 
     export class Add {
         public static Dark = `${dark}/Actions/Add.svg`;
+        public static Muted = `${muted}/Actions/Add.svg`
         public static Light = '';
         public static Brand = `${brand}/Actions/Add.svg`;
     }

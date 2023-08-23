20fe682b46fc96d7450ba95ca9ebbfc9cf3ee289
bogdan.aldea <bogdan.aldea@ROMOB41181>
2023-08-24 00:51:25 +0300

    Completed setup step

--- client/src/app/views/components/fields/fields.module.ts
+++ client/src/app/views/components/fields/fields.module.ts
@@ -2,18 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FieldComponent } from './field/field.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 
 
 @NgModule({
   declarations: [
-    FieldComponent
+    FieldComponent,
+    DatePickerComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatInputModule,
   ],
-  exports: [FieldComponent]
+  exports: [
+    FieldComponent,
+    DatePickerComponent
+  ]
 })
 export class FieldsModule { }
